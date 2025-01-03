1891eb92f3808663fc64480e06447245716d9278
eeorprogrammer <edgaremilio83@gmail.com>
2025-01-03 16:03:56 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Counter from './counter/counter'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App
