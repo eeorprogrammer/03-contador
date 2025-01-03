27a0afe075d3ec254a30ef8307651ac1897aba44
eeorprogrammer <edgaremilio83@gmail.com>
2025-01-03 16:29:00 -0600

    adding counter component

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 function Counter() {
-    return(
+    // Hooks Area
+    const [contador, setContador] = useState(0);
+    return (
         <div>
-            
+            <p>
+                Haz realizado click {contador} veces en este boton
+            </p>
+            <button onClick={() => { setContador(contador + 1) }}>
+                Incrementar
+            </button>
+
         </div>
     );
 
